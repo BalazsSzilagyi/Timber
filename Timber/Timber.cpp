6f4ac09f70556ae5065127540442b2bdb13d0540
Balazs Szilagyi <n0812461@ntu.ac.uk>
2019-09-26 21:00:51 +0100

    V. 0.12
    - Basic main game loop added.
    - In game background added.

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -1,7 +1,9 @@
 //Include important C++ libraries.
-
+//The double quation is for including files within Visual Studio
 #include "pch.h"
 #include <iostream>
+
+//Tells the pre-processor to include the Graphics.hpp
 #include <SFML/Graphics.hpp>
 
 // Mak code easier to type with "using namespace"
@@ -14,5 +16,53 @@ int main()
 
 	//Create and open a window from the game
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+	//Create a texture to hold a graphic on the GPU
+	Texture textureBackground;
+
+	//Load a graphic into the texture
+	textureBackground.loadFromFile("graphics/background.png");
+
+	//Create a sprite
+	Sprite spriteBackground;
+	
+	//Attach the texture to the sprite
+	spriteBackground.setTexture(textureBackground);
+
+	//Set the spriteBackground to cover the screen
+	spriteBackground.setPosition(0, 0);
+
+	while (window.isOpen())
+	{
+		/*
+		*********************************************************
+		Handle the players input
+		*********************************************************
+		*/
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		/*
+		*********************************************************
+		Update the scene
+		*********************************************************
+		*/
+
+		/*
+		*********************************************************
+		Draw the scene
+		*********************************************************
+		*/
+
+		//Clear everything from the last frame
+		window.clear();
+
+		//Draw our game scene here
+		window.draw(spriteBackground);
+
+		//Show everything we just drew
+		window.display();
+	}
 	return 0;
 }
