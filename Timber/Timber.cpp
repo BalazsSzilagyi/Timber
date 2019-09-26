ea2bf8c823af4b2b25606265a206f89b7a70faa2
Balazs Szilagyi <n0812461@ntu.ac.uk>
2019-09-26 13:09:32 +0100

    V 0.11
    Basic settings and window created.

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -1,10 +1,18 @@
-//This file contains the 'main' function. Program execution begins and ends there.
-//
+//Include important C++ libraries.
 
 #include "pch.h"
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+// Mak code easier to type with "using namespace"
+using namespace sf;
 
 int main()
 {
+	// Create a video mode object
+	VideoMode vm(1920, 1080);
+
+	//Create and open a window from the game
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
 	return 0;
 }
